78b8422745bd0f1a2fbdfab4e6e6543258270b14
gbhutra <mail.gbhutra@gmail.com>
2016-04-09 00:23:35 +0000

    bug fix for the event w.r.t pg database

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,7 @@ class Event < ActiveRecord::Base
         @events = Event.order('id desc').all
     end
     
-    scope :past_events,->{ where('date < ?', Date.today) }
-    scope :future_events,->{ where('date > ?', Date.today) }
-    scope :ongoing_events,->{ where('date == ?', Date.today) }
+    #scope :past_events,->{ where('date < ?', Date.today) }
+    #scope :future_events,->{ where('date > ?', Date.today) }
+    #scope :ongoing_events,->{ where('date == ?', Date.today) }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+Event.create!(title: "New Event", description: "New Event", venue: "514C", date: "2016-04-09", start_time: "1", end_time: "2", ticket_price: "5")
