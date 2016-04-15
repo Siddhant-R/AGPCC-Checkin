c1054aabb0555fd3f71e7031cde74579b796d644
gbhutra <mail.gbhutra@gmail.com>
2016-04-15 01:59:24 +0000

    Database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,5 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
-Event.create!(title: "New Event _4", description: "This is new event #4", venue: "College Station", ticket_price: "5$", created_at: "2016-04-09 21:09:56", updated_at: "2016-04-09 21:09:56", start_time: "2000-01-01 15:14:00", end_time: "2000-01-01 18:00:00", longitude: -96.3344068, latitude: 30.627977)
+Event.create!(title: "Current Event", description: "This is an on going Event", venue: "WCL, Texas A&M University, College Station, Texas,...", ticket_price: "20$", created_at: "2016-04-12 01:44:14", updated_at: "2016-04-12 01:44:14", longitude: -96.3579087, latitude: 30.5894709, start_time: "2016-04-11 20:00:00", end_time: "2017-04-11 18:00:00") 
+Event.create!(title: "Future Event", description: "This is a future Event", venue: "HRBB, College Station, TX", ticket_price: "25", created_at: "2016-04-12 01:46:43", updated_at: "2016-04-12 01:59:57", longitude: -96.3387678, latitude: 30.6189722, start_time: "2017-04-11 10:00:00", end_time: "2017-04-13 08:00:00")
