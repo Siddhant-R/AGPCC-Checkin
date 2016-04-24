f97a50e8059d111735efc9472822f9839b6d5a01
gbhutra <mail.gbhutra@gmail.com>
2016-04-24 04:52:53 +0000

    drop down for members

--- app/admin/member.rb
+++ app/admin/member.rb
@@ -11,6 +11,18 @@ permit_params :email, :first_name, :last_name, :gender, :classification
      actions
 end
 
+form do |f|
+ f.inputs "Member Details" do
+  f.input :first_name
+  f.input :last_name
+  f.input :email
+  f.input :gender,  as: :select, collection: Member::GENDER
+  f.input :classification,  as: :select, collection: Member::CLASSIFICATION
+ end
+  f.actions
+end
+
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
