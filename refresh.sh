c661359f7ca3608dc1907484e2d60f486a3fad79
Siddhant <sid@tamu.edu>
2016-05-03 00:31:34 +0000

    Partials: /analyze/index

--- refresh.sh
+++ refresh.sh
@@ -1,3 +1,15 @@
+#local variables
+bold=$(tput bold)
+normal=$(tput sgr0)
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+BLUE='\033[1;34m'
+BLOCK_BLUE='\e[44m'
+BLOCK_NORMAL='\e[49m'
+#Header
+printf "%b" "\n${BLOCK_BLUE}                    --- REMEBER SID :P  ---        ${BLOCK_NORMAL}\n"
+bundle install
 sudo service postgresql restart
 rake db:reset
 rake db:migrate
