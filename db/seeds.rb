27e63912eaca23b03089384cb0a1ee2658cc086f
gbhutra <mail.gbhutra@gmail.com>
2016-05-04 17:42:17 +0000

    seed fils

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
-Event.create!(title: "Current Event", description: "This is an on going Event", venue: "WCL, Texas A&M University, College Station, Texas,...", created_at: "2016-04-12 01:44:14", updated_at: "2016-04-12 01:44:14", longitude: -96.3579087, latitude: 30.5894709, start_time: "2016-04-11 20:00:00", end_time: "2017-04-11 18:00:00") 
-Event.create!(title: "Future Event", description: "This is a future Event", venue: "HRBB, College Station, TX", created_at: "2016-04-12 01:46:43", updated_at: "2016-04-12 01:59:57", longitude: -96.3387678, latitude: 30.6189722, start_time: "2017-04-11 10:00:00", end_time: "2017-04-13 08:00:00")
 
 Member.create!(email: "sid@example.com", first_name: "Siddhant", last_name: "Rath", gender: "Male", classification: "Master", paid_status: true, points: 0)
 Member.create!(email: "gsam@example.com", first_name: "Ghanshyam", last_name: "Bhutra", gender: "Male", classification: "Master", paid_status: false, points: 0) 
